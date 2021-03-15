83d118604fdcac81765d489c3f93cf53975c146e
Non Ratchanon <80709320+NonRatchanon@users.noreply.github.com>
2021-03-16 01:00:44 +0700

    Update Juice.java

--- GroupHW/6250110009/Juice.java
+++ GroupHW/6250110009/Juice.java
@@ -1,10 +1,3 @@
-/* 
-6250110001  Kampee Kantangkun
-6250110002  Natthakarn khunsaman
-6250110004  Norapich Pongsuwan
-6250110005  Panathanan Patcharaprasertsuk
-6250110009  Ratchanon  Songmeuang  
-*/
 
 public class Juice extends Product{
 
