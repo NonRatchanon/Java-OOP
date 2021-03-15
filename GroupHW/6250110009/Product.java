c25c25dfa7756c453c2ac1355e98a5cccb968390
Non Ratchanon <80709320+NonRatchanon@users.noreply.github.com>
2021-03-16 01:01:31 +0700

    Update Product.java

--- GroupHW/6250110009/Product.java
+++ GroupHW/6250110009/Product.java
@@ -1,10 +1,4 @@
-/* 
-6250110001  Kampee Kantangkun
-6250110002  Natthakarn khunsaman
-6250110004  Norapich Pongsuwan
-6250110005  Panathanan Patcharaprasertsuk
-6250110009  Ratchanon  Songmeuang  
-*/
+
 
 public abstract class Product {
     private String productName;
